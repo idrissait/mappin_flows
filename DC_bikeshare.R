fcd662f94557774e74a2855e5d822d3a0e32ad64
idrissait <idriss.ni@gmail.com>
2019-06-03 18:34:54 +0200

    Create DC_bikeshare.R

---
+++ DC_bikeshare.R
@@ -0,0 +1,36 @@
+library(plyr)
+library(ggplot2)
+library(maptools)
+library(plyr)
+
+input<-read.table("~/Downloads/201904-capitalbikeshare-tripdata.csv", sep=",", header=T)
+
+input<- input[,1:3]
+names(input)<- c("origin", "destination","total")
+
+centroids<- read.csv("~/Downloads/Capital_Bike_Share_Locations.csv")
+
+or.xy<- merge(input, centroids, by.x="Start.station", by.y="ADDRESS")
+
+
+destination.xy<-  merge(or.xy, centroids, by.x="End.station", by.y="ADDRESS")
+
+dest.xy <- subset(destination.xy, select = c(LATITUDE.x, LONGITUDE.x, LATITUDE.y, LONGITUDE.y, Member.type, Duration))
+
+names(dest.xy)<- c("oX", "oY", "dX", "dY", "type", "Duration")
+
+
+dest.xy <- ddply(dest.xy,~oX + oY + dX + dY + type,summarise,Duration=sum(Duration))
+
+
+xquiet<- scale_x_continuous("", breaks=NULL)
+yquiet<-scale_y_continuous("", breaks=NULL)
+quiet<-list(xquiet, yquiet)
+
+ggplot(dest.xy, aes(oX, oY))+
+  #The next line tells ggplot that we wish to plot line segments. The "alpha=" is line transparency and used below 
+  geom_segment(aes(x=oX, y=oY,xend=dX, yend=dY, alpha=Duration), col="white")+
+  #Here is the magic bit that sets line transparency - essential to make the plot readable
+  scale_alpha_continuous(range = c(0.01, 0.1))+
+  #Set black background, ditch axes and fix aspect ratio
+  theme(panel.background = element_rect(fill='black',colour='black'))+quiet+coord_equal()
